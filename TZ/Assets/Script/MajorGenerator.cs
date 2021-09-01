539ea6935bc87b1e2f44326ffc7328e4003928da
FatFat <dima.stolyarov2013@yandex.ru>
2021-09-01 23:23:05 +0700

    MajorGenerator
    Секретная разработка Российских военных . Генератор Майоров

---
+++ TZ/Assets/Script/MajorGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MajorGenerator : MonoBehaviour
+{
+    public GameObject major;
+    Vector2 point;
+    Vector2 difference = new Vector2(0,4);
+    void Generation(Vector2 point)
+    {
+        Instantiate(major, point - difference, Quaternion.identity);
+        Instantiate(major, point + difference, Quaternion.identity);
+    }
+    private void Start()
+    {
+        point = new Vector2(10,Random.Range(0,3));
+        Generation(point);
+    }
+}
---
+++ TZ/Assets/Script/MajorMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MajorMove : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position -= transform.right * Time.deltaTime*2;
+        if(GetComponent<Transform>().position.x < - 9)
+        {
+            Destroy(gameObject); //За военкоматом нет жизни
+        }
+    }
+}
