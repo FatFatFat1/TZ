36181d6325ccd586521a51fec4d039e2700c4807
FatFat <dima.stolyarov2013@yandex.ru>
2021-09-01 20:23:07 +0700

    character jump+animation
    прыжки персонажа и его анимация

---
+++ TZ/Assets/Script/CharacterMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    float gravity;
+    private Animator anim;
+    void Start()
+    {
+        gravity = GetComponent<Rigidbody2D>().gravityScale;
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            gravity = -gravity;
+            GetComponent<Rigidbody2D>().gravityScale = gravity;
+            anim.SetBool("IsJump", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            gravity = -gravity;
+            GetComponent<Rigidbody2D>().gravityScale = gravity;
+            anim.SetBool("IsJump", false);
+        }
+    }
+}
