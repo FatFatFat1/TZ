af09f20194f997c013c40d08ea3eb4d9b2c7c19a
FatFat <dima.stolyarov2013@yandex.ru>
2021-09-03 20:33:40 +0700

    SaveAndReadRecordInFile
    Запись и чтение рекорда
    Рекорд сохраняется между сессиями

--- TZ/Assets/Script/ScopeAdd.cs
+++ TZ/Assets/Script/ScopeAdd.cs
@@ -1,22 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 public class ScopeAdd : MonoBehaviour
 {
     private GameObject player;
+    int record;
+    int scope;
+    string path = @"D:\FlappyBird\TZ\TZ\Assets\Script\record.txt";
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        using (FileStream recordSave = new FileStream(path, FileMode.Open))
+        {
+            byte[] array = new byte[recordSave.Length];
+            recordSave.Read(array, 0, array.Length);
+            string textFromFile = System.Text.Encoding.Default.GetString(array);
+            record = Convert.ToInt32(textFromFile);
+            player.GetComponent<ScopeData>().record = record;
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        int record = player.GetComponent<ScopeData>().record;
-        int scope = player.GetComponent<ScopeData>().scope;
+        scope = player.GetComponent<ScopeData>().scope;
         scope += 1;
         if (scope > record)
         {
             player.GetComponent<ScopeData>().record = scope;
+            using (FileStream recordSave = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(scope.ToString());
+                recordSave.Write(array, 0, array.Length);
+            }
         }
         player.GetComponent<ScopeData>().scope = scope;
     }
--- TZ/Assets/Script/ScopeData.cs
+++ TZ/Assets/Script/ScopeData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class ScopeData : MonoBehaviour
 {
@@ -9,9 +10,17 @@ public class ScopeData : MonoBehaviour
     public int record;
     public GameObject scopeBoard;
     public GameObject recordBoard;
+    string path = @"D:\FlappyBird\TZ\TZ\Assets\Script\record.txt";
     private void Update()
     {
         scopeBoard.GetComponent<Text>().text = scope.ToString();
         recordBoard.GetComponent<Text>().text = record.ToString();
+        using (FileStream recordSave = new FileStream(path, FileMode.Open))
+        {
+            byte[] array = new byte[recordSave.Length];
+            recordSave.Read(array, 0, array.Length);
+            string textFromFile = System.Text.Encoding.Default.GetString(array);
+            //Debug.Log(($"Текст из файла: {textFromFile}"));
+        }
     }
 }
