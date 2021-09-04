c6368263f533a780d6a94144712a8459706a4594
FatFat <dima.stolyarov2013@yandex.ru>
2021-09-04 15:29:07 +0700

    MainMenu
    Сцена главного меню
    Возможность заходить в главное меню из экрана поражения

---
+++ TZ/Assets/Script/LoseScreenButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseScreenButton : MonoBehaviour
+{
+    public void QuitGamePressed()
+    {
+        SceneManager.LoadScene("Main Menu");
+        SceneManager.UnloadSceneAsync("SampleScene");
+    }
+
+}
