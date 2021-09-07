0b74416b05fcc6cd973c950e9913edd33ecab01a
FatFat <dima.stolyarov2013@yandex.ru>
2021-09-07 19:17:42 +0700

    Забыл сделать паузу
    Пауза

--- TZ/Assets/Script/CharacterMove.cs
+++ TZ/Assets/Script/CharacterMove.cs
@@ -23,6 +23,11 @@ public class CharacterMove : MonoBehaviour
 
     #endregion Private Fields
 
+    /// <summary>
+    /// Чтобы персонаж не прыгал сквозь паузу
+    /// </summary>
+    static public bool IsPause = false;
+
     #region Private Methods
 
     private void Start()
@@ -33,29 +38,35 @@ public class CharacterMove : MonoBehaviour
     private void Update()
     {
 #if UNITY_STANDALONE
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (!IsPause)
         {
-            Player.AddForce(transform.up * 10.0f, ForceMode2D.Impulse);
-            _anim.SetBool("IsJump", true);
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            _anim.SetBool("IsJump", false);
-        }
-#elif UNITY_IOS || UNITY_ANDROID
-         if(Input.touchCount > 0)
-         {
-            Touch myTouch =  Input.GetTouch(0);
-            if(myTouch.phase == TouchPhase.Began)
-             {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
                 Player.AddForce(transform.up * 10.0f, ForceMode2D.Impulse);
                 _anim.SetBool("IsJump", true);
-             }
-             if(myTouch.phase == TouchPhase.Ended)
-             {
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
                 _anim.SetBool("IsJump", false);
+            }
+        }
+#elif UNITY_IOS || UNITY_ANDROID
+        if (!IsPause)
+        {
+             if(Input.touchCount > 0)
+             {
+                Touch myTouch =  Input.GetTouch(0);
+                if(myTouch.phase == TouchPhase.Began)
+                 {
+                    Player.AddForce(transform.up * 10.0f, ForceMode2D.Impulse);
+                    _anim.SetBool("IsJump", true);
+                 }
+                 if(myTouch.phase == TouchPhase.Ended)
+                 {
+                    _anim.SetBool("IsJump", false);
+                 }
              }
-         }
+        }
 #endif
     }
 
---
+++ TZ/Assets/Script/Pause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Скрипт постановки игры на паузу
+/// </summary>
+public class Pause : MonoBehaviour
+{
+    /// <summary>
+    /// То что мы присваем для Time.timeScale
+    /// </summary>
+    public float Timer;
+    /// <summary>
+    /// Стоит ли пауза?
+    /// </summary>
+    public bool IsPause = false;
+    /// <summary>
+    /// Для того чтобы запретить игроку перемещаться (я не знаю как он сквозь паузу все равно прыгает)
+    /// </summary>
+    public void PausePressed()
+    {
+        IsPause = !IsPause;
+        if (IsPause)
+        {
+            Timer = 0;
+            CharacterMove.IsPause = true;
+        }
+        else if (!IsPause)
+        {
+            Timer = 1f;
+            CharacterMove.IsPause = false;
+        }
+        Time.timeScale = Timer;
+    }
+}
